58114b11eadea4b589ac344a6d91dfd21ae34938
camharmon <cameronrainharmon@gmail.com>
2020-10-31 13:46:56 -0400

    First commit

---
+++ src/build/precache-manifest.090d6582f43e62b8dd2e85ae50f361dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623f92cefcc5c4fb69381de706d55983",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e875b5f1b87e50a6ced",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "79294f2972c18d11563d",
+    "url": "/static/js/2.637d709d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.637d709d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e875b5f1b87e50a6ced",
+    "url": "/static/js/main.0843a536.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
